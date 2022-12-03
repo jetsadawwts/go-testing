f9252f0ee18447a7301dc0db367cc21bd804ed62
jetsadawwts <jetsadawwts@gmail.com>
2022-12-03 13:18:54 +0700

    Ep1 go-testing

---
+++ sum/sum_test.go
@@ -0,0 +1,54 @@
+package sum
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	// t.Log("testing sum")
+	// t.Error("testing sum")
+
+	t.Run("should return 3 when 1 and 2", func(t *testing.T) {
+		// Arrange
+		want := 3
+		
+
+		//Act
+		got := sum(1, 2)
+
+
+		//Assert
+		if got != want {
+			t.Errorf("sum(1, 2) = %d; want 3", got)
+		}
+	})
+
+	t.Run("should return -2 when -1 and -1", func(t *testing.T) {
+		
+		// Arrange
+		want := -2
+
+		//Act
+		got := sum(-1, -1)
+
+		//Assert
+		if got != want {
+			t.Errorf("sum(-1, -1) = %d; want -2", got)
+		}
+	})
+	
+}
+
+// func TestSumShouldReturn1WhenInput1And0(t *testing.T) {
+// 	got := sum(1, 0)
+
+// 	if got != 1 {
+// 		t.Errorf("sum(1, 0) = %d; want 1", got)
+// 	}
+// }
+
+// func TestSumShouldReturnMinus2WhenInputMinus1AndMinus2(t *testing.T) {
+// 	got := sum(-1, -1)
+
+// 	if got != -2 {
+// 		t.Errorf("sum(-1, -1) = %d; want -2", got)
+// 	}
+// }
